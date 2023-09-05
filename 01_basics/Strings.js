f6bf423b428c461fffa2fa8e8ad69e73f1c0352a
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-05 03:01:29 +0000

    Strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,26 @@
+const name = "hitesh"
+const repoCount = 50
+//console.log(name + repoCount + " Value")//do not use this type of coding
+
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);//use this type of coding
+
+const gameName = new String('hitesh-hc-com')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase())
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+//console.log(newString)
+const newStringg = gameName.slice(2,-4)
+//console.log(newStringg)
+
+const anotherString = "    audumbar   "
+//console.log(anotherString.trim())
+
+
+
+
