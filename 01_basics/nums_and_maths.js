e2b50cef5bad4266a2bd947475a05b4583b49ac9
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-07 03:37:58 +0000

    Maths_and_nums

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,16 @@
+/* console.log(Math)
+console.log(Math.abs(-4))//absolute value print
+console.log(Math.round(4.6))//round it to one number
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))//round it to lower value
+console.log(Math.min(4,3,6,8))//min number
+console.log(Math.max(4,3,6,8))//max number */
+
+console.log(Math.random())
+console.log(Math.random()*10 + 1)//to get the value greater than 0/ to avoid 0 value 
+console.log(Math.floor(Math.random()*10) + 1)// round it to one number and add min value
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min +1))+min)
+
