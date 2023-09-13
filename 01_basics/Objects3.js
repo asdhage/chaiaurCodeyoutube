92735350116f752e41b345526536f87554e09051
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-13 02:13:16 +0000

    Object&API short

---
+++ 01_basics/Objects3.js
@@ -0,0 +1,23 @@
+ const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+ }
+
+ //console.log(course.courseInstructor);//nprmal accessing method
+const {courseInstructor: instruct} = course//acccessing using assign mehtod
+console.log(instruct);
+
+//API discussion
+{//declarinf API and assigning values to it
+    "name":"hitesh",
+    "coursename":"js in hindi"
+    "price":"free"
+}
+
+//array API
+[
+    {},
+    {},
+    {},
+]
