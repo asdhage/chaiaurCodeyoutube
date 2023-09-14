da2ca1f4315126cc90618deaf1dfc47b817a6f28
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-14 03:53:40 +0000

    scope1

---
+++ 01_basics/scope.js
@@ -0,0 +1,10 @@
+
+//var c = 300
+if(true){
+    let a = 10
+    const b =20
+    var c = 30
+}
+//console.log(a);//a will not print
+//console.log(b);//b will not print
+console.log(c);//c prints as decalred as var
