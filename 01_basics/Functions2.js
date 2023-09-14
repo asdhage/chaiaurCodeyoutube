ec05c06d78ce1a26544340d4f666ea10a0538c17
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-14 03:35:23 +0000

    functions2

---
+++ 01_basics/Functions2.js
@@ -0,0 +1,34 @@
+/* function calculateCartPrice(...num1){//... means rest operator it will add all values into array 
+    return num1
+}
+console.log(calculateCartPrice(200,300,400));
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,2000));//it will print 400 and 2000 other values are stored in val1 and val2
+ */
+
+
+/* const user = {
+    username : "hitesh",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject({
+    username:"sam",
+    price:399
+}) */
+
+c/* onst myNewArray = [200,400,100,600]//array declaration
+function returnSecondValue(getArray){//function call
+    return getArray[0]
+}
+//console.log(returnSecondValue(myNewArray));//print the values
+console.log(returnSecondValue([200,300,400])); */
+
+
+
+
