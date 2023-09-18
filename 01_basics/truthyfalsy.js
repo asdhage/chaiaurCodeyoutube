a6571d20c977993535afbae6a9433bd53f6830ff
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-18 03:34:35 +0000

    truthyfalsy

---
+++ 01_basics/truthyfalsy.js
@@ -0,0 +1,38 @@
+
+//falsy values
+//false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+//truthy values
+//"0",'false'," ",[],{},function(){}
+
+/* if(userEmail.length==0){//used to compare using lenth of array or string
+    console.log("Array is empty");
+} */
+
+/* const emptyObj = {}
+
+if (Object.keys(emptyObj).length ===0){//object key compare using length
+    console.log("Object is empty");
+} */
+
+/* //nullish coalescing operator (??): numm undefined
+
+let val1;
+//val1 =5 ?? 10//5 output
+//val1 = null ?? 10//10 output
+//val1 = undefined ?? 20//20 output
+ val1 = null ?? 10 ?? 20// 10 ouput
+
+console.log(val1); */
+
+//ternary operator
+//condition ? true : false//syntax
+
+const iceTean = 100
+
+iceTean <=80 ? console.log("less than 80"):console.log("more thean 80");
+
+
+
+
+
