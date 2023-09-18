98354ec1ea11717092dbc351e7aba5da2312e98e
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-18 02:53:45 +0000

    ifelse

---
+++ 01_basics/controlFlow.js
@@ -0,0 +1,44 @@
+//if
+
+if(true){//if true then code will execute otherwise not
+
+}
+
+//if (2 == "2"){//it will print as true for that purpose we need to use === operator to comprare string and numbers
+//if (2 === "2"){//it will not execute the loop as condition is false
+if (2 == 3){//this one is question type input 
+    console.log("executed");
+}
+
+/* const temp = 23
+if(temp == 23){
+    console.log("temp is less");
+}
+else{
+    console.log("temp is high");
+} */
+
+/* const score = 200
+if (score >100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+} */
+
+const balance = 1000
+//if(balance >500) console.log("test"),console.log("test2");//don't write these type of code it will terminate you from job
+
+const userlogged = true
+const userid = false
+
+if(userlogged || userid){
+    console.log("welcome");
+}
+
+
+
+
+
+
+
+
+
