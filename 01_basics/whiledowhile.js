2a4e6a455377c3f2c98c8364b8d37de34bb6a479
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-22 03:09:12 +0000

    whiledowhile

---
+++ 01_basics/whiledowhile.js
@@ -0,0 +1,22 @@
+/* let index =0 
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    index = index+2
+} */
+
+/* let myarr=["Audu","Digu","Prasad"]
+let arrr = 0
+while (arrr < myarr.length) {
+    console.log(`the value is ${myarr[arrr]}`);
+    arrr = arrr+1
+} */
+
+// dowhileloop
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++//print first and then stops the loop
+} while (score<=10);
+
+
