caba9390fc4984e1d9e7f4f43ce352cc251191d8
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-22 03:32:14 +0000

    forofloop

---
+++ 01_basics/forofloop.js
@@ -0,0 +1,40 @@
+//for of 
+
+/* let arr = [0,1,2,3,4,5]
+for (const log of arr) {
+    console.log(log);
+} */
+
+/* const greet = "Hello world"
+for (const gree of greet) {
+    console.log(`each character is ${gree}`);
+} */
+
+
+//MAPS
+
+const map = new Map()//map will not allow duplicate entrys
+map.set('IN',"India")
+map.set('Dist',"Solapur")
+map.set('Vil',"Nimboni")
+map.set('Vil',"Nimboni")
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-', value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
