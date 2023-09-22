2233a49e6ac563e268316b3b3ed1954847246c43
Audumbar Dhage <58381533+asdhage@users.noreply.github.com>
2023-09-22 05:12:05 +0000

    forinloop

---
+++ 01_basics/objectsandloops.js
@@ -0,0 +1,13 @@
+const myobject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb:"rubby",
+    swift:"swift by apple"
+}
+for (const key in myobject) {
+    console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+
+
+
